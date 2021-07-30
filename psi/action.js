3e7f2749d4a3689017e43459f84d86cff1ea56d5
Chris Millar <cmillar@adobe.com>
2021-07-30 15:24:56 -0600

    f

--- psi/action.js
+++ psi/action.js
@@ -4,12 +4,28 @@ const { getPsi } = require('./lib/psi');
 const buildBody = require('./lib/buildBody');
 const getColor = require('./lib/getColor');
 
+function getResults(lh, fcp, lcp, tbt, cls) {
+  return {
+    lh: { label: 'LH', value: lh, color: getColor('lh', lh, true) },
+    fcp: { label: 'FCP', value: fcp, color: getColor('fcp', fcp) },
+    lcp: { label: 'LCP', value: lcp, color: getColor('lcp', lcp) },
+    tbt: { label: 'TBT', value: tbt, color: getColor('tbt', tbt) },
+    cls: { label: 'CLS', value: cls, color: getColor('cls', cls) },
+  };
+}
+
+async function getSecondAttempt(url, psiKey) {
+  const { lh, fcp, lcp, tbt, cls } = await getPsi(url, psiKey);
+  return getResults(lh, fcp, lcp, tbt, cls);
+}
+
 async function run() {
   try {
     const token = core.getInput('repo-token');
     const psiKey = core.getInput('psi-key');
     const relativeUrl = core.getInput('relative-url');
 
+    const lhThreshold = core.getInput('lh');
     const fcpThreshold = core.getInput('fcp');
     const lcpThreshold = core.getInput('lcp');
     const tbtThreshold = core.getInput('tbt');
@@ -22,16 +38,21 @@ async function run() {
     const url = 'https://www.adobe.com/';
 
     const { lh, fcp, lcp, tbt, cls } = await getPsi(url, psiKey);
-    
-    const results = {
-      lh: { label: 'LH', value: lh, color: getColor('lh', lh, true) },
-      fcp: { label: 'FCP', value: fcp, color: getColor('fcp', fcp) },
-      lcp: { label: 'LCP', value: lcp, color: getColor('lcp', lcp) },
-      tbt: { label: 'TBT', value: tbt, color: getColor('tbt', tbt) },
-      cls: { label: 'CLS', value: cls, color: getColor('cls', cls) },
-    };
+    const results = getResults(lh, fcp, lcp, tbt, cls);
 
-    const body = buildBody(url, results);
+    let secondResults;
+    if (lh < lhThreshold ||
+        fcp > fcpThreshold ||
+        lcp > lcpThreshold ||
+        tbt > tbtThreshold ||
+        cls > clsThreshold ) {
+        secondResults = await getSecondAttempt(url, psiKey);
+    }
+
+    const body = buildBody(url, results, 1, true);
+    if (secondResults) {
+      body += buildBody(url, secondResults, 2, false);
+    }
 
     const issue_number = github.context.payload.pull_request.number;
     const owner = github.context.repo.owner;
